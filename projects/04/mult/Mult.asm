3f9b055a1a081a0f902b029be5fe5df3c4ae2d5c
Akifumi Fujita <akifuji@protonmail.com>
2022-01-18 13:13:50 +0900

    complete Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,29 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @sum
+    M=0
+    @0
+    D=M
+    @i
+    M=D
+(LOOP)
+    @i
+    D=M
+    @END
+    D;JEQ
+    @1
+    D=M
+    @sum
+    M=D+M
+    @i
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @sum
+    D=M
+    @2
+    M=D
+    @END
+    0;JMP
