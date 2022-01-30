a68b527f5ae794bca25dcc4e448d1ffd2605ab16
Akifumi Fujita <akifuji@protonmail.com>
2022-01-30 10:58:08 +0900

    implemented StackAtirhmetic

---
+++ projects/07/code_writer.py
@@ -0,0 +1,115 @@
+from enum import Enum, auto
+
+
+class CommandType(Enum):
+    C_ARITHMETIC = auto()
+    C_PUSH = auto()
+    C_POP = auto()
+    C_LABEL = auto()
+    C_GOTO = auto()
+    C_IF = auto()
+    C_FUNCTION = auto()
+    C_RETURN = auto()
+    C_CALL = auto()
+
+
+class CodeWriter:
+    def __init__(self, file_name):
+        self.output_file = file_name.split(".")[0] + ".asm"
+        self.label = 0
+        open(self.output_file, "w")
+
+    def write(self, line):
+        with open(self.output_file, "a") as f:
+            f.write(line)
+
+    def convert(self, line, command_type) -> str:
+        args = line.split()
+        converted = ""
+        if command_type == CommandType.C_ARITHMETIC:
+            converted = self.convert_arithmetic(args)
+        elif command_type == CommandType.C_PUSH or command_type == CommandType.C_POP:
+            converted = self.convert_push_pop(args)
+        return converted
+
+    def convert_arithmetic(self, args) -> str:
+        command = args[0]
+        asm = ""
+        if command in ["add", "sub", "eq", "lt", "gt", "and", "or"]:
+            asm = self.binary_arithmetic_operator(asm)
+            if command == "add":
+                asm += "M=D+M\n"
+            elif command == "sub":
+                asm += "M=M-D\n"
+            elif command == "and":
+                asm += "M=M&D\n"
+            elif command == "or":
+                asm += "M=M|D\n"
+            elif command in ["eq", "lt", "gt"]:
+                asm = self.comparison_operator(asm, command)
+            asm = self.decrease_stack_pointer(asm)
+        elif command in ["neg", "not"]:
+            asm = self.unary_arithmetic_operator(asm)
+            if command == "neg":
+                asm += "M=-M\n"
+            elif command == "not":
+                asm += "M=!M\n"
+        return asm
+
+    @staticmethod
+    def decrease_stack_pointer(asm) -> str:
+        asm += "@SP\n"
+        asm += "M=M-1\n"
+        return asm
+
+    @staticmethod
+    def unary_arithmetic_operator(asm) -> str:
+        asm += "@SP\n"
+        asm += "A=M-1\n"
+        return asm
+
+    @staticmethod
+    def binary_arithmetic_operator(asm) -> str:
+        asm += "@SP\n"
+        asm += "A=M-1\n"
+        asm += "D=M\n"
+        asm += "A=A-1\n"
+        return asm
+
+    def comparison_operator(self, asm, command) -> str:
+        asm += "D=M-D\n"
+        asm += "@TRUE_{}\n".format(self.label)
+        if command == "eq":
+            asm += "D;JEQ\n"
+        elif command == "lt":
+            asm += "D;JLT\n"
+        elif command == "gt":
+            asm += "D;JGT\n"
+        asm += "@SP\n"
+        asm += "A=M-1\n"
+        asm += "A=A-1\n"
+        asm += "M=0\n"
+        asm += "@END_{}\n".format(self.label)
+        asm += "0;JMP\n"
+        asm += "(TRUE_{})\n".format(self.label)
+        asm += "@SP\n"
+        asm += "A=M-1\n"
+        asm += "A=A-1\n"
+        asm += "M=-1\n"
+        asm += "(END_{})\n".format(self.label)
+        self.label += 1
+        return asm
+
+    @staticmethod
+    def convert_push_pop(args) -> str:
+        asm = ""
+        if args[0] == "push":
+            if args[1] == "constant":
+                asm += "@{}\n".format(args[2])
+                asm += "D=A\n"
+                asm += "@SP\n"
+                asm += "A=M\n"
+                asm += "M=D\n"
+                asm += "@SP\n"
+                asm += "M=M+1\n"
+        return asm
